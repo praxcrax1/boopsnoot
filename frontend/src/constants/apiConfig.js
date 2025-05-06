37c1bbbc07db1e040fa1ff910e5d241c0dd57124
Prakhar Gupta <prakharyologupta@gmail.com>
2025-05-07 01:28:57 +0530

    new client id

--- frontend/src/constants/apiConfig.js
+++ frontend/src/constants/apiConfig.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://boopsnoot.onrender.com/api';
 export const SOCKET_URL = API_URL.replace('/api', '');
 export const GOOGLE_CLIENT_ID = '474314594002-tqidlcfif5aal78ap613f1jc1mp4oum6.apps.googleusercontent.com';
-export const GOOGLE_ANDROID_CLIENT_ID = '474314594002-apopigau24c6kfjqkut44ed0f07j1o4j.apps.googleusercontent.com';
+export const GOOGLE_ANDROID_CLIENT_ID = '474314594002-e7s4qnre9g4k2drv7v1b94vhjt5i9anh.apps.googleusercontent.com';
 export const GOOGLE_ANDROID_REDIRECT_URI = 'com.praxcrax.boopsnoot://oauth2redirect/google';
